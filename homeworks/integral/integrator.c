1ebc39f8872776f9b8fa4832c23e16b12745caf9
Jens <au636450@uni.au.dk>
2021-06-12 16:13:43 +0200

    Curtis Clenshaw routine implemented

--- homeworks/integral/integrator.c
+++ homeworks/integral/integrator.c
@@ -2,33 +2,53 @@
 #include <math.h>
 #include <assert.h>
 
-
-double x_i[] = {1.0/6, 2.0/6, 4.0/6, 5.0/6};
-double w_i[] = {2.0/6, 1.0/6, 1.0/6, 2.0/6};
-double v_i[] = {1.0/4, 1.0/4, 1.0/4, 1.0/4};
+ 
+double x_i[] = {1.0/6, 2.0/6, 4.0/6, 5.0/6}; // Abssicas, for easy re use when subdividing intervals
+double w_i[] = {2.0/6, 1.0/6, 1.0/6, 2.0/6}; // Weigths (trapezium rule)
+double v_i[] = {1.0/4, 1.0/4, 1.0/4, 1.0/4}; // Weights for error estimate (rectangle rule)
 
 double adapt24(double f(double), double a, double b, double acc, double eps, double f2, double f3, int limit ){
 
     assert (limit < 1e5);
 
+    // Calculate end points
     double f1 = f(a + (b-a) * x_i[0]);
     double f4 = f(a + (b-a) * x_i[3]);
 
-    double Q = (2 * f1 + f2 + f3 + 2 * f4) / 6 * (b-a);
+    // Calculate the 2. and 4. order estimate and the tolerance and error
+    double Q = (2 * f1 + f2 + f3 + 2 * f4) / 6 * (b-a); 
     double q = (f1 + f4 + f2 + f3) / 4 * (b-a);
     double tolerance = acc + eps * fabs(Q);
     double error = fabs(Q-q);
 
+    // Check if step is accepted or rejected
     if (error < tolerance) { return Q; }
-    else {
+    else { //subdivide if rejected
         double Q1 = adapt24(f, a, (a+b)/2, acc/sqrt(2.0), eps, f1, f2, limit+1); 
         double Q2 = adapt24(f, (a+b)/2, b, acc/sqrt(2.0), eps, f3, f4, limit+1); 
         return Q1 + Q2; 
     }
 }
 double quad(double f(double), double a, double b, double acc, double eps) {
+    // Calculate middle points 
     double f2 = f(a + 2 * (b-a) / 6);
     double f3 = f(a + 4 * (b-a) / 6); 
     int limit = 0;
     return adapt24(f, a, b, acc, eps, f2, f3, limit); 
+}
+
+
+// Curtis Clenshaw transformation of the integral
+
+double (*cc_func)(double);
+double cc_a, cc_b;
+double cc_wrapper(double x) {
+    return cc_func((cc_a + cc_b)/2 + (cc_b - cc_a)*cos(x)/2)*(cc_b - cc_a)*sin(x)/2;
+}
+
+double quadCC(double f(double), double a, double b, double acc, double eps){
+    cc_func = f;
+    cc_a = a;
+    cc_b = b;
+    return quad(cc_wrapper, 0, M_PI, acc, eps);  
 }
--- homeworks/integral/main.c
+++ homeworks/integral/main.c
@@ -3,11 +3,12 @@
 #include <assert.h>
 
 double quad(double f(double), double a, double b, double acc, double eps); // Header for quadrature function
+double quadCC(double f(double), double a, double b, double acc, double eps);
 
 int calls;
 double pi = 3.14159265359;
 
-double sqrt_x(double x) { 
+double sqrt(double x) { 
     calls++;
     return sqrt(x); 
 }
@@ -17,16 +18,58 @@ double weird_function(double x) {
     return 4 * sqrt(1 - x*x);
 }
 
+double inverse_sqrt(double x) {
+    calls++;
+    return 1.0/sqrt(x);
+}
+
+double log_inverse_sqrt(double x) {
+    calls++;
+    return log(x)/sqrt(x);
+}
+
+
 int main() {
+
+/* -------- PART A ----------- */
+
+    printf("PART A\n");
+    printf("\n");
+    printf("Adaptive 24 integration routine\n");
+    printf("\n");
     calls = 0;
-    double quad1 = quad(sqrt_x, 0, 1, 0.001, 0.001);
+    double quad1 = quad(sqrt, 0, 1, 0.001, 0.001);
     printf("Value of the integral of sqrt(x)dx from 0 to 1 is (should be 2/3) %10.10f\n", quad1);
     printf("Number of calls: %10i\n", calls);
     
     printf("\n");
     calls = 0;
-    double quad2 = quad(weird_function, 0, 1, 0.001, 0.001);
-    printf("Value of the integral of 4*sqrt(1-x^2)dx from 0 to 1 is (should be pi = 3.14159265359) %10.10f\n", quad2);
+    double quad2 = quad(weird_function, 0, 1, 0.00001, 0.00001);
+    printf("Value of the integral of 4*sqrt(1-x^2)dx from 0 to 1 is (should be pi = 3.14159265359) %10.15f\n", quad2);
     printf("Number of calls: %10i\n", calls);
+    printf("\n");
+/* -------- PART B ----------- */
+
+    printf("PART B\n");
+    printf("\n");
+    printf("Curtis Clenshaw 24 integration routine\n");
+    printf("\n");
+    calls = 0;
+    double quad3 = quadCC(inverse_sqrt, 0, 1, 0.001, 0.001);
+    printf("Value of the integral of 1/sqrt(x) from 0 to 1 is (should be 2) %10.10f\n", quad3);
+    printf("Number of calls: %10i\n", calls);
+
+    printf("\n");
+    calls = 0;
+    double quad4 = quadCC(log_inverse_sqrt, 0, 1, 0.001, 0.001);
+    printf("Value of the integral of ln(x)/sqrt(x) from 0 to 1 is (should be -4) %10.10f\n", quad4);
+    printf("Number of calls: %10i\n", calls);
+
+    printf("\n");
+    calls = 0;
+    double quad5 = quadCC(weird_function, 0, 1, 0.00001, 0.00001);
+    printf("Value of the integral of 4*sqrt(1-x^2)dx from 0 to 1 is (should be pi = 3.14159265359) %10.15f\n", quad5);
+    printf("Number of calls: %10i\n", calls);
+
     return 0;
 }
